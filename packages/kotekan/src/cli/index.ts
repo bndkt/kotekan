46500a36c4ea134696cd6a66f12d91d44aaa095d
Benedikt Müller <91166910+bndkt@users.noreply.github.com>
2024-03-01 22:38:42 +0800

    Test

--- packages/kotekan/src/cli/index.ts
+++ packages/kotekan/src/cli/index.ts
@@ -1,19 +1,24 @@
 #!/usr/bin/env bun
-// --hot
 import { ssrServer } from "../ssr/server";
-import { rnServer } from "../rn/server";
+// import { rnServer } from "../rn/server";
 
 const development = Bun.env.NODE_ENV === "development";
 
-const ssr = await ssrServer({
-	mode: "ssr",
-	development,
-});
+// const ssr = await ssrServer({
+// 	mode: "csr",
+// 	development,
+// });
 
 // const rn = await rnServer({
 // 	port: 8081,
 // 	development
 // });
+
+const ssr = Bun.serve({
+	fetch(req) {
+		return new Response("Bun!");
+	},
+});
 
 console.log(
 	`Listening on {rsc.url} (RSC) and ${ssr.url} (SSR) and {rn.url} (RN) ${
