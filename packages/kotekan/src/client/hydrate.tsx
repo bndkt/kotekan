790c1c3b32b5a45abed15b9c667f14f45511d8c4
Benedikt Müller <91166910+bndkt@users.noreply.github.com>
2024-03-05 09:57:44 +0800

    Update hydrate

--- packages/kotekan/src/client/hydrate.tsx
+++ packages/kotekan/src/client/hydrate.tsx
@@ -2,11 +2,21 @@
 import { StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 // @ts-expect-error Untyped import
-import { createFromFetch } from "react-server-dom-esm/client";
+import { createFromFetch } from "react-server-dom-esm/client"; // @todo
+// import { createFromFetch } from "react-server-dom-webpack/client.browser";
 
 import { setupNavigation } from "./setupNavigation";
 
-const hydrate = await createFromFetch(fetch("?jsx"));
+const hydrate = await createFromFetch(
+	fetch("?jsx", {
+		headers: {
+			Accept: "text/x-component",
+		},
+	}),
+	{
+		moduleBaseURL: "/BASEEEEEEEE",
+	},
+);
 const root = hydrateRoot(document, <StrictMode>{hydrate}</StrictMode>);
 
 setupNavigation(root);
