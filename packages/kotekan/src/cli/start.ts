f75bb666f6e7a8f8a273ac3da02dc5af8446e229
Benedikt Müller <91166910+bndkt@users.noreply.github.com>
2024-03-07 08:27:01 +0800

    Preserve env vars

--- packages/kotekan/src/cli/start.ts
+++ packages/kotekan/src/cli/start.ts
@@ -4,17 +4,18 @@ import path from "node:path";
 export const startCommand = async () => {
 	const jsxServerPath = path.join(import.meta.dir, "jsxServer.ts");
 	const jsxServerPort = 3001;
-
 	const jsxServer = Bun.spawn(
 		["bun", "--conditions", "react-server", jsxServerPath],
-		{ env: { PORT: jsxServerPort.toString() } },
+		{
+			env: { PORT: jsxServerPort.toString(), ...process.env },
+			stdout: "inherit",
+		},
 	);
 
 	const ssrServerPath = path.join(import.meta.dir, "ssrServer.ts");
 	const ssrServerPort = 3000;
-
 	const ssrServer = Bun.spawn(["bun", ssrServerPath], {
-		env: { PORT: ssrServerPort.toString() },
+		env: { PORT: ssrServerPort.toString(), ...process.env, stdout: "inherit" },
 	});
 
 	console.log(`Kotekan running at http://localhost:${ssrServerPort}`);
