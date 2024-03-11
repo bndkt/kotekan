937a1f4dcb4d042bc1a5f3ae91ec28dd7a6d9969
Benedikt Müller <91166910+bndkt@users.noreply.github.com>
2024-03-11 13:22:52 +0800

    Update route

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -4,13 +4,8 @@ import { HeroPattern } from "../components/layout/HeroPattern";
 export default function Index() {
 	return (
 		<>
-			<title>Home - Kotekan</title>
+			<title>Kotekan</title>
 			<HeroPattern />
-			<h1>Hello, world!</h1>
-			<a href="/about">About</a>
-			<p className="text-red-500">
-				This should be red if Tailwind works 23456.
-			</p>
 			<Counter />
 		</>
 	);
