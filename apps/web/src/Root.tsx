7cd2d4e32ae35d7979f74d465f6593f3cca9648e
Benedikt Müller <91166910+bndkt@users.noreply.github.com>
2024-03-11 20:16:25 +0800

    Add font

--- apps/web/src/Root.tsx
+++ apps/web/src/Root.tsx
@@ -10,6 +10,11 @@ export default function Root({ children }: { children?: ReactNode }) {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<meta name="description" content="Kotekan" />
 				<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/css?family=Inter&display=swap"
+				/>
 			</head>
 			<body className="flex min-h-full bg-white antialiased dark:bg-zinc-900">
 				<div className="w-full">
