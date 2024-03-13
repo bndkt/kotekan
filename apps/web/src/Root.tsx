76896ffa9efa61eafcc5fda25da95bb5a74a9ac1
Benedikt Müller <91166910+bndkt@users.noreply.github.com>
2024-03-13 13:35:19 +0800

    Add og image

--- apps/web/src/Root.tsx
+++ apps/web/src/Root.tsx
@@ -8,13 +8,25 @@ export default function Root({ children }: { children?: ReactNode }) {
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<meta name="description" content="Kotekan" />
+				<meta
+					name="description"
+					content="Kotekan is a modern React Server Components framework built on Bun."
+				/>
 				<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" />
 				<link
 					rel="stylesheet"
 					href="https://fonts.googleapis.com/css?family=Inter&display=swap"
 				/>
+				<meta
+					property="og:title"
+					content="Kotekan - React Server Components framework built on Bun"
+				/>
+				<meta
+					property="og:description"
+					content="Kotekan is a modern React Server Components framework built on Bun."
+				/>
+				<meta property="og:image" content="https://kotekan.dev/og-image.png" />
 			</head>
 			<body className="flex min-h-full bg-white antialiased dark:bg-zinc-900">
 				<div className="w-full">
