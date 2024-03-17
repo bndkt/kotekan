a6684760a4a813d4e0ac2ab6377e1bc9e0bb99ae
Benedikt Müller <91166910+bndkt@users.noreply.github.com>
2024-03-18 06:36:46 +0800

    Upgrade Bun

--- packages/kotekan/src/cli/jsxServer.ts
+++ packages/kotekan/src/cli/jsxServer.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env bun --conditions react-server --preload ../../packages/kotekan/src/cli/preload-jsx.ts
+#!/usr/bin/env -S bun --conditions react-server --preload ./node_modules/kotekan/src/cli/jsxPreload.ts
 import { server } from "../server";
 
 const development = Bun.env.NODE_ENV === "development";
