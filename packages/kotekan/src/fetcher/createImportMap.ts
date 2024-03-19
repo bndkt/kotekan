40ca2b66dfaa956643d843cf13a27310067a466c
Benedikt Müller <91166910+bndkt@users.noreply.github.com>
2024-03-19 13:16:56 +0800

    Upgrade dependencies

--- packages/kotekan/src/fetcher/createImportMap.ts
+++ packages/kotekan/src/fetcher/createImportMap.ts
@@ -2,7 +2,7 @@ import type { ImportMap } from "./types";
 
 export const createImportMap = (development?: boolean): ImportMap => {
 	const jsxRuntimeImport = development ? "jsx-dev-runtime" : "jsx-runtime";
-	const reactVersion = "0.0.0-experimental-eb33bd747-20240312";
+	const reactVersion = "0.0.0-experimental-a870b2d54-20240314";
 	const queryString = `?pin=v135${development ? "&dev" : ""}`;
 
 	const importMap: ImportMap = {
