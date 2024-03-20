88cbee535440edf11f80e1f1511e093fea3c0830
Benedikt Müller <91166910+bndkt@users.noreply.github.com>
2024-03-20 15:39:46 +0800

    Re-add demo, write announcement

---
+++ apps/web/src/routes/demo/index.tsx
@@ -0,0 +1,14 @@
+import { Suspense } from "react";
+
+import { Wrapper } from "@/components/content/Wrapper";
+import { ServerComponent } from "../../components/demo/ServerComponent";
+
+export default function Demo() {
+	return (
+		<Wrapper>
+			<title>Demo - Kotekan</title>
+			<h1>Demo</h1>
+			<p>Various demos showcasing Kotekan features.</p>
+		</Wrapper>
+	);
+}
---
+++ apps/web/src/routes/demo/pokemon/index.tsx
@@ -0,0 +1,17 @@
+import { Suspense } from "react";
+
+import { Wrapper } from "@/components/content/Wrapper.tsx";
+import { PokemonList } from "../../../components/demo/PokemonList.tsx";
+
+export default function Pokemon() {
+	return (
+		<Wrapper>
+			<title>Pokémon - Kotekan</title>
+			<h1>Kotekan - Pokémon</h1>
+			<Suspense fallback={<div>Loading Pokémon &hellip;</div>}>
+				{/* @ts-expect-error Async component */}
+				<PokemonList />
+			</Suspense>
+		</Wrapper>
+	);
+}
